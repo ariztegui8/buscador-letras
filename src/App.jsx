ec755923e7b572724d3ccde817de35da479613fe
ariztegui8 <jorgeariztegui8@gmail.com>
2022-06-25 02:01:51 -0300

    primer commit letras

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import AppLetras from "./components/AppLetras";
+
+
+function App() {
+  return (
+    <div>
+        <AppLetras />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AppLetras.jsx
@@ -0,0 +1,18 @@
+import Formulario from "./Formulario"
+
+
+const AppLetras = () => {
+  return (
+    <>
+        <header>Búsqueda de Letras de Canciones</header>
+
+        <Formulario />
+
+        <main className="container p-3 bg-white">
+            
+        </main>
+    </>
+  )
+}
+
+export default AppLetras
